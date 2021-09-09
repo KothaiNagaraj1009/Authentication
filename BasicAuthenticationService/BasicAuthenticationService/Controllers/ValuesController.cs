d89a533a8c689b6678dfbb791f2e3c846481b12f
kothai <kothaiveradis@gmail.com>
2021-09-09 04:57:16 -0700

    Initial checkin

---
+++ BasicAuthenticationService/BasicAuthenticationService/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using BasicAuthenticationService.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAuthenticationService.Controllers
+{
+    [BasicAuthentication]
+    [RequireHttps]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+
+        // POST api/values
+        public HttpResponseMessage Post()
+        {
+            var response = Request.CreateResponse<string>(System.Net.HttpStatusCode.Created, "Your first POST request!");
+            return response;
+        }
+        public HttpResponseMessage Post(int id)
+        {
+
+            var response = Request.CreateResponse<string>(System.Net.HttpStatusCode.Created, id.ToString());
+            return response;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [System.Web.Http.FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BasicAuthenticationService/BasicAuthenticationService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasicAuthenticationService.WebApiApplication" Language="C#" %>
